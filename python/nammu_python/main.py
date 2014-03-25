5ad642af7506f90995ecb8a79bf9e1b642545ee6
James Hetherington <j.hetherington@ucl.ac.uk>
2014-03-25 16:44:02 +0000

    Prove GitHub integration via Java works

--- python/nammu_python/main.py
+++ python/nammu_python/main.py
@@ -5,18 +5,25 @@ Created on 25 Mar 2014
 '''
 
 from javax.swing import JButton, JFrame
+from org.kohsuke.github import GitHub
+
+gh=GitHub.connect()
+me=gh.getMyself()
+
 
 frame = JFrame('Hello, Jython!',
             defaultCloseOperation = JFrame.EXIT_ON_CLOSE,
             size = (300, 300)
         )
 
-def change_text(event):
-    print 'Clicked!'
+button = JButton('Click to get your name from GitHub!')
 
-button = JButton('Click Me!', actionPerformed=change_text)
 frame.add(button)
 
+def change_text(event):
+    button.setText(me.name)
+
+button.actionPerformed=change_text
 
 def main():
     print "Python called"
