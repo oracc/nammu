389474eda00a79b9c13b6ed0b2dcb8fc59d4759e
raquel-ucl <r.alegre@ucl.ac.uk>
2015-04-29 12:52:55 +0100

    The systemState's path and prefix don't need modification now that the maven configuration has been fixed.

--- src/main/java/uk/ac/ucl/rc/development/oracc/nammu/App.java
+++ src/main/java/uk/ac/ucl/rc/development/oracc/nammu/App.java
@@ -3,6 +3,7 @@
  * 
  * It sets up the Jython interpreter and ensures that it can find any requisite 
  * external libraries.
+ * It call Python's entry point nammu.main.main()
  * 
  * @author raquel-ucl
  * 
@@ -10,29 +11,16 @@
 
 package uk.ac.ucl.rc.development.oracc.nammu;
 
-import java.util.Properties;
-
 import org.python.core.Py;
 import org.python.core.PyException;
-import org.python.core.PyString;
 import org.python.core.PySystemState;
 import org.python.util.PythonInterpreter;
 
 public class App {
 	
 	 public static void main(String[] args) throws PyException {
-	        PySystemState.initialize(
-	            PySystemState.getBaseProperties(), 
-	            new Properties(), args);
-
-	        PythonInterpreter interpreter = new PythonInterpreter();
-	        
 	        PySystemState systemState = Py.getSystemState();
-
-	        //Link some external python libraries installed with a 
-	        // non-configurable maven plugin during mvn install/package
-	        systemState.path.append(new PyString("target/classes/Lib"));
-	        
+	        PythonInterpreter interpreter = new PythonInterpreter();
 	        systemState.__setattr__("_jy_interpreter", Py.java2py(interpreter));
 	        interpreter.exec("try:\n import nammu.main\n nammu.main.main()\nexcept SystemExit: pass");
 	    }
