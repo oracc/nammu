57bc05edcac5bf6c5f64ee92bc213291f4fecdac
Jens Hedegaard Nielsen <jens.nielsen@ucl.ac.uk>
2016-01-27 14:41:06 +0000

    Remove pyoracc copy

--- python/pyoracc/model/link.py
+++
@@ -1,5 +0,0 @@
-class Link(object):
-    def __init__(self, label, code, description):
-        self.label = label
-        self.code = code
-        self.description = description
--- python/pyoracc/model/score.py
+++
@@ -1,7 +0,0 @@
-from mako.template import Template
-
-class Score(object):
-    def __init__(self, ttype, mode, word=False):
-        self.ttype = ttype
-        self.mode = mode
-        self.word = word
