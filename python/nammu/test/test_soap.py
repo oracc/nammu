a83cd34a72ae9506f0ca423d713788fa487cba6a
Raquel Alegre <r.alegre@ucl.ac.uk>
2016-02-25 11:18:20 +0000

    Some dummy tests for the SOAP client added.

---
+++ python/nammu/test/test_soap.py
@@ -0,0 +1,28 @@
+import pytest
+from ..controller.SOAPClient import SOAPClient
+from ..controller.HTTPRequestBuilder import HTTPRequestBuilder
+
+class TestSOAP:
+    def setup(self):
+        pass
+
+    def teardown(self):
+        pass
+
+    def test_send_request(self):
+        pass
+
+    def test_get_response_ok(self):
+        pass
+
+    def test_create_soap_envelope(self):
+        pass
+
+    def test_headers(self):
+        pass
+
+    def test_body(self):
+        pass
+
+    def test_response_ready(self):
+        pass
