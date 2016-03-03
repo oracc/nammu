1c6bb823e4cb6b513357ae5aaf88121a7643ec3a
Raquel Alegre <r.alegre@ucl.ac.uk>
2016-03-03 10:20:28 +0000

    Trying to use pytest from within jython and be run by mvn test.

---
+++ src/test/java/uk/ac/ucl/rc/development/oracc/nammu/AppTest.java
@@ -0,0 +1,28 @@
+package uk.ac.ucl.rc.development.oracc.nammu;
+
+import org.python.core.Py;
+import org.python.core.PyException;
+import org.python.core.PySystemState;
+import org.python.util.PythonInterpreter;
+
+/**
+ * This is the entry point for Nammu's tests.
+ *
+ * It sets up the Jython interpreter and ensures that it can find py.test.
+ * It calls Py.test to run all the python tests in Nammu.
+ *
+ */
+public class AppTest {
+
+    public void testApp() throws PyException {
+      PySystemState systemState = Py.getSystemState();
+      PythonInterpreter interpreter = new PythonInterpreter();
+      systemState.__setattr__("_jy_interpreter", Py.java2py(interpreter));
+      String command = "try:\n "
+                     + "  import pytest\n "
+                     + "  pytest.main()\n"
+                     + "except "
+                     + "  SystemExit: pass";
+      interpreter.exec(command);
+    }
+}
