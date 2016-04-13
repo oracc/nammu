5469b311ac2e14abbbc1b71077a1b9121c3f82bf
Raquel Alegre <r.alegre@ucl.ac.uk>
2016-04-13 16:58:41 +0100

    Added event listener that calculates the char position in the text based on the x,y position of the mouse.

--- python/nammu/view/AtfEditArea.py
+++ python/nammu/view/AtfEditArea.py
@@ -1,5 +1,6 @@
 from java.awt import Font
 from javax.swing import JTextPane, BorderFactory
+from java.awt.event import MouseAdapter
 
 
 class AtfEditArea(JTextPane):
@@ -8,3 +9,13 @@ class AtfEditArea(JTextPane):
         self.font = Font("Monaco", Font.PLAIN, 14)
         # Tooltip
         self.setToolTipText("Hello")
+
+        listener = CustomMouseListener(self)
+        self.addMouseListener(listener)
+
+class CustomMouseListener(MouseAdapter):
+    def __init__(self, panel):
+        self.panel = panel
+    def mousePressed(self, event):
+        offset = self.panel.viewToModel(event.getPoint())
+        print offset
