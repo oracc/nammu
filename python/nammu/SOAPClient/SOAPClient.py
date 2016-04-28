323474166b7ff25fd245dbb5a23a690b5ce045c6
Raquel Alegre <r.alegre@ucl.ac.uk>
2016-04-28 17:27:46 +0100

    Pass URL, port and URL dir separately to the SOAP client. Also removed some unused stuff.

--- python/nammu/SOAPClient/SOAPClient.py
+++ python/nammu/SOAPClient/SOAPClient.py
@@ -4,18 +4,20 @@ import re
 import StringIO
 from zipfile import ZipFile
 import xml.etree.ElementTree as ET
-import httplib as http_client
 from HTTPRequest import HTTPRequest
 
 class SOAPClient(object):
     """
     Sends and retrieves information to and from the ORACC SOAP server.
     """
-    def __init__(self, url, method):
+    def __init__(self, url, port, url_dir, method):
         self.url = url
+        self.port = port
+        self.url_dir = url_dir
         self.method = method
         logging.basicConfig()
         self.logger, self.request_log = self.setup_logger()
+
 
     def setup_logger(self):
         """
@@ -29,25 +31,25 @@ class SOAPClient(object):
         request_log.propagate = True
         return logger, request_log
 
+
     def create_request(self, **kwargs):
         request = HTTPRequest(self.url, self.method, **kwargs)
         self.request = request
+
 
     def send(self):
         """
         Elaborate HTTP POST request and send it to ORACC's server.
         """
-        url = "http://oracc.museum.upenn.edu:8085"
+        url = "{}:{}".format(self.url, self.port)
         headers = dict(self.request.get_headers())
         body = self.request.get_body()
         self.response = requests.post(url, data=body, headers=headers)
-        # try:
-        #    response = requests.post(url, data=body, headers=headers, timeout=10)
-        # except ReadTimeout:
-        #          print "Timed out!"
+        
 
     def get_response_text(self):
         return self.response.text
+
 
     def get_response_id(self):
         xml_root = ET.fromstring(self.response.text)
@@ -55,35 +57,28 @@ class SOAPClient(object):
         # here: https://docs.python.org/2/library/xml.etree.elementtree.html
         return xml_root[0][0][0][0].text
 
-    def wait_for_response(self, id):
+
+    def wait_for_response(self, request_id):
         """
         Check for a response to the request and obtain response zip file.
+        Since it's an asynchronous communication, when sending a request for
+        validation or lemmatisation, the server returns an ID for us to ask
+        via HTTP GET until the request is ready.
+        The response from the server can be:
+        * "run\n" (request is being processed)
+        * "done\n" (request is ready - we can send a new SOAP request to get it)
+        *  "err_stat\n" (something bad happened and we have to mail Steve)
         """
+        url = "{}/{}/{}".format(self.url, self.url_dir, request_id)
         while True:
-            ready_response = requests.get('http://oracc.museum.upenn.edu/p/' + id)
-            if ready_response.text == "done\n":
+            response = requests.get(url)
+            if response.text == "done\n":
                 return
+
 
     def get_response(self):
         return self.response.content
 
-    def parse_response(self):
-        """
-        Extract information sent in server response.
-        """
-        pass
-
-    def _check_response_ready(self, id):
-        """
-        Send a HTTP GET request to ORACC's server and retrieve status.
-        """
-        pass
-
-    def create_request_zip(self):
-        """
-        Pack attachment in a zip file.
-        """
-        pass
 
     def get_server_logs(self):
         """
@@ -102,11 +97,12 @@ class SOAPClient(object):
         oracc_log = zip_content['oracc.log']
         request_log = zip_content['request.log']
         # See if server returns a lemmatised file
-        autolem = None
+        autolem = None 
         for key, value in zip_content.iteritems():
             if key.endswith("autolem.atf"):
-                autolem = zip_content[key]
+                autolem = value
 
+        print zip_content.keys()
         print "@"*30
         print oracc_log
         print "@"*30
@@ -117,8 +113,3 @@ class SOAPClient(object):
             print "@"*30
 
         return oracc_log, request_log, autolem
-
-    def get_autolem(self):
-        """
-
-        """
