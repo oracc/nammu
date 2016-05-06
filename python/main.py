2b3b94418402930bdb03bf7c2fe9ba2c070bb20c
Raquel Alegre <r.alegre@ucl.ac.uk>
2016-05-06 17:32:45 +0100

    Trying to capture errors coming from communication with the ORACC server.

---
+++ python/main.py
@@ -0,0 +1,16 @@
+'''
+Created on 15 Apr 2015
+
+Python entry point invoked from Java's entry point.
+
+@author: raquel-ucl
+'''
+
+from nammu.controller.NammuController import NammuController
+
+def main():
+    NammuController()
+
+
+if __name__ == '__main__':
+    main()
