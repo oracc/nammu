48c3c85aa9372c39a89fbf079b0d0f7b5bb43edd
Raquel Alegre <r.alegre@ucl.ac.uk>
2016-06-15 14:26:53 +0100

    Clear tooltips and line number colouring from previous files or validations.

--- python/nammu/controller/AtfAreaController.py
+++ python/nammu/controller/AtfAreaController.py
@@ -50,11 +50,19 @@ class AtfAreaController(object):
         return self.view.editArea.getText()
 
     def clearAtfArea(self):
-        self.view.editArea.setText("")
         # When opening a new file we should discard the previous edits
         self.view.undo_manager.discardAllEdits()
         # Reload line numbers text panel
         self.view.repaint_line_numbers(0)
+        # Clear tooltips
+        self.clearToolTips()
+        
+    def clearToolTips(self):
+        '''
+        We don't want tooltips from previous validations appearing after the
+        file has been re-validated or another file has been opened.
+        '''
+        self.view.editArea.setToolTipText(None)
 
     def update_line_numbers(self):
         # Get how many lines are in the file
