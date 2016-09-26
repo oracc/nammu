6aee8f1490e7c416b0a8aa3086271fb366dda304
Benjamin A. Laken <benlaken@gmail.com>
2016-09-26 16:03:59 +0100

    forgot a file

---
+++ python/nammu/test/test_yaml_update.py
@@ -0,0 +1,36 @@
+'''
+Copyright 2015, 2016 University College London.
+
+This file is part of Nammu.
+
+Nammu is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Nammu is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Nammu.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+import pytest
+import os
+#from ..utils import update_yaml_config
+
+@pytest.mark.skip(reason=("pyyaml is not working in the tests yet"))
+def test_update_yaml_config():
+    """
+    Ensure that upon updating yaml settings files from jar, a users
+    default project settings are not overwritten.
+    """
+    d = update_yaml_config(path_to_jar="resources/test/jar_settings.yaml",
+                         yaml_path="resources/test/user_settings.yaml",
+                         path_to_config="resources/test/user_settings.yaml",
+                         test_mode=True)
+
+    #assert goal_setting == jar_config
+    # make sure the user (project) setting is not overwritten....
