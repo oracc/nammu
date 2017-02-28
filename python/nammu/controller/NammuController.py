bf4f219dc20a49727657456eb8f64a053e1ee049
Raquel Alegre <r.alegre@ucl.ac.uk>
2017-02-28 19:54:23 +0000

    Read server url, port and dir from config file.

--- python/nammu/controller/NammuController.py
+++ python/nammu/controller/NammuController.py
@@ -461,10 +461,10 @@ class NammuController(object):
         '''
         # Build request.zip on the fly, pack all needed in it and send to
         # server
-        # TODO: Would be nice these are read from config file.
-        url = 'http://oracc.museum.upenn.edu'
-        port = 8085
-        url_dir = 'p'
+        server = self.config.servers['default']
+        url = self.config.servers[server].url
+        port = self.config.servers[server].port
+        url_dir = self.config.servers[server].dir
 
         # Create HTTP client and prepare all input arguments for request
         client = SOAPClient(url, port, url_dir, method='POST')
