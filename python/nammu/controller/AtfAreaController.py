c16a7a91582409ea6a95a0eeff85551b22c139c5
Stuart Grieve <s.grieve@ucl.ac.uk>
2017-07-12 16:39:03 +0100

    fixing indenting istake

--- python/nammu/controller/AtfAreaController.py
+++ python/nammu/controller/AtfAreaController.py
@@ -184,20 +184,20 @@ class AtfAreaController(object):
         return pos
 
     def getLinePositions(self, text):
-    '''
-    Given a block of text, return the caret positions
-    at the start and end of each line as a list of tuples in the order
-    (start, end) assuming left to right text.
+        '''
+        Given a block of text, return the caret positions
+        at the start and end of each line as a list of tuples in the order
+        (start, end) assuming left to right text.
 
-    The hacky list slicing is to handle off by one errors as the first line
-    starts at position 0, whereas every other line starts at +1 past the end of
-    the last line.
-    '''
-    if len(text) > 0:
-        compiled = re.compile(r"\n")
-        textiter = compiled.finditer(text)
-        pos = [m.start() for m in textiter][1:]
-    else:
-        return [(0, 0)]
+        The hacky list slicing is to handle off by one errors as the first line
+        starts at position 0, whereas every other line starts at +1 past the
+        end of the last line.
+        '''
+        if len(text) > 0:
+            compiled = re.compile(r"\n")
+            textiter = compiled.finditer(text)
+            pos = [m.start() for m in textiter][1:]
+        else:
+            return [(0, 0)]
 
-    return zip([0] + [x + 1 for x in pos], pos)
+        return zip([0] + [x + 1 for x in pos], pos)
