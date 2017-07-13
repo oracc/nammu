32df95e0e3d73b6d9d566721426b6d831c8f5e96
Stuart Grieve <s.grieve@ucl.ac.uk>
2017-07-13 13:08:18 +0100

    Removing unneeded import

--- python/nammu/view/AtfAreaView.py
+++ python/nammu/view/AtfAreaView.py
@@ -17,7 +17,6 @@ You should have received a copy of the GNU General Public License
 along with Nammu.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-import re
 from java.awt import BorderLayout, Dimension, Color
 from java.awt.event import KeyListener
 from javax.swing import JScrollPane, JPanel, JSplitPane
