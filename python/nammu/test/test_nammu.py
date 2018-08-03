0997e2d48d877a9c44188d24e1cee6b7a44f78f4
Stuart Grieve <s.grieve@ucl.ac.uk>
2018-08-03 15:36:46 +0100

    adding the test file

---
+++ python/nammu/test/test_nammu.py
@@ -0,0 +1,19 @@
+import pytest
+from python.nammu.controller.NammuController import NammuController
+
+
+
+class TestNammu(object):
+
+    def setup_method(self):
+
+        self.nammu = NammuController()
+
+    def teardown_method(self):
+        self.nammu.quit()
+
+
+    def test_set_text(self):
+        self.nammu.atfAreaController.edit_area.setText('Hello!')
+
+        assert self.nammu.atfAreaController.edit_area.getText() == 'Hello!'
