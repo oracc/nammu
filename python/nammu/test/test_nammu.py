cbc239a7a3bdd60d54b4f4f9bd378bdd2a49e903
Raquel Alegre <r.alegre@ucl.ac.uk>
2018-08-03 15:46:48 +0100

    Fix setup and teardown methods.

--- python/nammu/test/test_nammu.py
+++ python/nammu/test/test_nammu.py
@@ -5,15 +5,12 @@ from python.nammu.controller.NammuController import NammuController
 
 class TestNammu(object):
 
-    def setup_method(self):
-
+    def setup_method(self, method):
         self.nammu = NammuController()
 
-    def teardown_method(self):
+    def teardown_method(self, method):
         self.nammu.quit()
-
 
     def test_set_text(self):
         self.nammu.atfAreaController.edit_area.setText('Hello!')
-
         assert self.nammu.atfAreaController.edit_area.getText() == 'Hello!'
