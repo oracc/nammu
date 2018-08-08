3e58e99aedf1cc4dbceb885c8b9f96007446c994
Stuart Grieve <s.grieve@ucl.ac.uk>
2018-08-08 13:49:56 +0100

    tests now load a series of atf files as fixtures

--- python/nammu/test/test_nammu.py
+++ python/nammu/test/test_nammu.py
@@ -1,6 +1,31 @@
+ # -*- coding: utf-8 -*-
 import pytest
 from python.nammu.controller.NammuController import NammuController
 
+
+@pytest.fixture
+def simpletext():
+    return 'this is a simple test line'
+
+@pytest.fixture
+def english():
+    with open('resources/test/english.atf') as f:
+        return f.read()
+
+@pytest.fixture
+def english_no_lem():
+    with open('resources/test/english_no_lem.atf') as f:
+        return f.read()
+
+@pytest.fixture
+def broken_atf():
+    with open('resources/test/english_broken.atf') as f:
+        return f.read()
+
+@pytest.fixture
+def arabic():
+    with open('resources/test/arabic.atf') as f:
+        return f.read()
 
 class TestNammu(object):
 
@@ -10,9 +35,8 @@ class TestNammu(object):
     def teardown_class(self):
         del self.nammu
 
-    def test_set_text(self):
-        self.nammu.atfAreaController.edit_area.setText('Hello!')
-        assert self.nammu.atfAreaController.edit_area.getText() == 'Hello!'
-
-    def test_set_text2(self):
-        assert 1 == 1
+    @pytest.mark.parametrize('text', [simpletext(), english(), arabic(),
+                                      english_no_lem(), broken_atf()])
+    def test_set_text(self, text):
+        self.nammu.atfAreaController.edit_area.setText(text)
+        assert self.nammu.atfAreaController.edit_area.getText() == text
