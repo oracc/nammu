7ffce31aa7b5048cc9b88be2c410704f7ccea3a0
Stuart Grieve <s.grieve@ucl.ac.uk>
2018-08-09 15:54:00 +0100

    work in progress got validation tests running

--- python/nammu/test/test_nammu.py
+++ python/nammu/test/test_nammu.py
@@ -1,31 +1,28 @@
  # -*- coding: utf-8 -*-
 import pytest
+import codecs
 from python.nammu.controller.NammuController import NammuController
 
 
 @pytest.fixture
 def simpletext():
-    return 'this is a simple test line'
+    return u'this is a simple test line'
 
 @pytest.fixture
 def english():
-    with open('resources/test/english.atf') as f:
-        return f.read()
+    return codecs.open('resources/test/english.atf', encoding='utf-8').read()
 
 @pytest.fixture
 def english_no_lem():
-    with open('resources/test/english_no_lem.atf') as f:
-        return f.read()
+    return codecs.open('resources/test/english_no_lem.atf', encoding='utf-8').read()
 
 @pytest.fixture
 def broken_atf():
-    with open('resources/test/english_broken.atf') as f:
-        return f.read()
+    return codecs.open('resources/test/english_broken.atf', encoding='utf-8').read()
 
 @pytest.fixture
 def arabic():
-    with open('resources/test/arabic.atf') as f:
-        return f.read()
+    return codecs.open('resources/test/arabic.atf', encoding='utf-8').read()
 
 class TestNammu(object):
 
@@ -40,3 +37,41 @@ class TestNammu(object):
     def test_set_text(self, text):
         self.nammu.atfAreaController.edit_area.setText(text)
         assert self.nammu.atfAreaController.edit_area.getText() == text
+
+    # def test_arabic_split_pane(self, arabic):
+    #     assert self.nammu.arabic_edition_on == False
+    #     self.nammu.atfAreaController.edit_area.setText(arabic)
+    #     assert self.nammu.arabic_edition_on == True
+
+    def test_syntax_highlight(self):
+        pass
+
+    def test_successful_lem(self):
+        pass
+
+    def test_unsuccsessful_lem(self):
+        pass
+
+    def test_saving_split_pane(self):
+        pass
+
+    @pytest.mark.parametrize('text', [english(), arabic(),
+                                      english_no_lem()])
+    def test_successful_validation(self, text):
+        self.nammu.currentFilename = 'pytest.atf'  # bypass saving the file
+        self.nammu.atfAreaController.edit_area.setText(text)
+        self.nammu.validate()
+
+        # An empty dictionary means no validation errors
+        assert not self.nammu.atfAreaController.validation_errors
+
+    def test_unsuccessful_validation(self, broken_atf):
+        self.nammu.currentFilename = 'pytest.atf'  # bypass saving the file
+        self.nammu.atfAreaController.edit_area.setText(broken_atf)
+        self.nammu.validate()
+
+        # An empty dictionary means no validation errors
+        assert self.nammu.atfAreaController.validation_errors
+
+    def test_file_load(self):
+        pass
